b19559a061dc8a77b45b3e133a08e4680fcbd3df
Александр Щербаков <a.v.shherbakov.2017@azure.nstu.ru>
2020-02-29 16:08:57 +0700

    ready constant

--- Translation methods/ConstantTable.h
+++ Translation methods/ConstantTable.h
@@ -12,15 +12,21 @@ public:
 	// void load(string filename);
 	void add(T element);
 	bool is_in_table(T element);
-	// string find_by_num(int num);
-	// int find_by_value(string element);
+	bool find_by_num(int num, T &value);
+	int find_by_value(T value);
 	ConstantTable();
-	// ~ConstantTable();
+	~ConstantTable();
 };
 
 template <typename T>
 ConstantTable<T>::ConstantTable()
 {}
+
+template <typename T>
+ConstantTable<T>::~ConstantTable()
+{
+	this->table.clear();
+}
 
 template <typename T>
 void ConstantTable<T>::add(T element)
@@ -32,4 +38,23 @@ template <typename T>
 bool ConstantTable<T>::is_in_table(T element)
 {
 	return this->table.find(element) != this->table.end();
+}
+
+template <typename T>
+bool ConstantTable<T>::find_by_num(int num, T &value)
+{
+	if (num < this->table.size())
+	{
+		value = *std::next(this->table.begin(), num);
+		return true;
+	}
+	return false;
+}
+
+template <typename T>
+int ConstantTable<T>::find_by_value(T value)
+{
+	auto found = this->table.find(value);
+	int index = std::distance(this->table.begin(), found);
+	return found != this->table.end() ? index : -1;
 }
--- Translation methods/main.cpp
+++ Translation methods/main.cpp
@@ -2,11 +2,19 @@
 #include <iostream>
 using std::cout;
 
-void main()
+void string_test()
 {
 	ConstantTable<string> table;
 	table.add("int");
 	table.add("float");
 	table.add("double");
-	cout << table.is_in_table("double");
+	bool in = table.is_in_table("double");
+	int int_index = table.find_by_value("int");
+	string int_str;
+	table.find_by_num(10, int_str);
+}
+
+void main()
+{
+	string_test();
 }
